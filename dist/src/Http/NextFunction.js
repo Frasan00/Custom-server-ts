dbf6189a21d51da980c22455d1a316abee0b6869
frasan00 <francesco.sangiovanni.7@gmail.com>
2023-06-26 08:57:19 +0200

    ignored and removed dist folder

--- dist/src/Http/NextFunction.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NextFunction = void 0;
-class NextFunction {
-}
-exports.NextFunction = NextFunction;
--- dist/src/Response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Response = void 0;
-class Response {
-}
-exports.Response = Response;
